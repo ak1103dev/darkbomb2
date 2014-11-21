b89092fcb8824b7c556f8bc0b8170e68af55d572
ak1103 <ak.apichan1103@gmail.com>
2014-11-21 22:29:03 +0700

    Add a Bomb When Finished

--- main.py
+++ main.py
@@ -16,18 +16,25 @@ class DarkBomb2Game(Board):
 
     def __init__(self):
         super(DarkBomb2Game, self).__init__('DarkBomb_2', DarkBomb2Game.BLACK, (440,440), 12)
-        self.bomb = Bomb(radius = 20,
-                        color = DarkBomb2Game.WHITE, 
-                        pos = (self.window_size[0]/2,self.window_size[1]/2))
-
-        self.reinit()
+        self.init_bombs()
+        self.init_player()
 
     def init(self):
         super(DarkBomb2Game, self).init()
 
     def reinit(self):
+        self.init_bombs()
+        self.init_player()
+
+    def init_player(self):
         self.player = Player(radius = 20, color = DarkBomb2Game.GREEN, pos = (20,20))
-        
+
+    def init_bombs(self):
+        self.bombs = [] 
+        self.bombs.append(Bomb(radius = 20,
+                        color = DarkBomb2Game.WHITE, 
+                        pos = (self.window_size[0]/2,self.window_size[1]/2))
+                        )
 
     def update(self):
         """ Player can't go out of screen """
@@ -51,8 +58,9 @@ class DarkBomb2Game(Board):
             self.player.move_left()
 
         """ Check Hit """
-        if self.bomb.check_hit(self.player):
-            DarkBomb2Game.game_over = True
+        for bomb in self.bombs:
+            if bomb.check_hit(self.player):
+                DarkBomb2Game.game_over = True
 
         """ Check Finished """
         if (self.player.x+20, self.player.y+20) == self.window_size:
@@ -67,14 +75,18 @@ class DarkBomb2Game(Board):
         """ Add Bomb When Game Finished """    
         if DarkBomb2Game.game_finish:
             print 'you win'
-            self.bomb.number += 1
-            self.reinit()
+            self.init_player()
+            self.bombs.append(Bomb(radius = 20,
+                        color = DarkBomb2Game.WHITE, 
+                        pos = (self.window_size[0]/2,self.window_size[1]/2))
+                        )
             DarkBomb2Game.game_finish = False
 
-        self.score_image = self.font.render('Bomb = %d' % self.bomb.number, 0, DarkBomb2Game.WHITE)
+        self.score_image = self.font.render('Bomb = %d' % len(self.bombs), 0, DarkBomb2Game.WHITE)
 
     def render(self, surface):
-        self.bomb.render(surface)
+        for bomb in self.bombs:
+            bomb.render(surface)
         self.player.render(surface)
         surface.blit(self.score_image, (self.window_size[0] - 120,10))
 
