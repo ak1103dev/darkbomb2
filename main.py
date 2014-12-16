67e4102fe20914ec8263912251fad98855ada2a6
ak1103 <ak.apichan1103@gmail.com>
2014-12-16 15:40:09 +0700

    stop when game over

--- main.py
+++ main.py
@@ -48,63 +48,64 @@ class DarkBomb2Game(Board):
 
     def update(self):
 
-        """ Player can't go out of screen """
-        self.player.in_screen(self.window_size)
+        if not DarkBomb2Game.game_over:
+            """ Player can't go out of screen """
+            self.player.in_screen(self.window_size)
 
-        """ Player's Movement """
-        if self.is_key_pressed(K_UP):
-            self.player.move_up()
-        elif self.is_key_pressed(K_DOWN):
-            self.player.move_down()
-        elif self.is_key_pressed(K_RIGHT):
-            self.player.move_rigth()
-        elif self.is_key_pressed(K_LEFT):
-            self.player.move_left()
+            """ Player's Movement """
+            if self.is_key_pressed(K_UP):
+                self.player.move_up()
+            elif self.is_key_pressed(K_DOWN):
+                self.player.move_down()
+            elif self.is_key_pressed(K_RIGHT):
+                self.player.move_rigth()
+            elif self.is_key_pressed(K_LEFT):
+                self.player.move_left()
 
-        """ Check Hit """
-        for bomb in self.bombs:
-            if bomb.check_hit(self.player):
-                DarkBomb2Game.game_over = True
+            """ Check Hit """
+            for bomb in self.bombs:
+                if bomb.check_hit(self.player):
+                    DarkBomb2Game.game_over = True
 
-        """ Check Finished """
-        if self.player.check_finished(self.window_size):
-            DarkBomb2Game.game_finish = True
+            """ Check Finished """
+            if self.player.check_finished(self.window_size):
+                DarkBomb2Game.game_finish = True
 
-        """ Check Closed """
-        for bomb in self.bombs:
-            if bomb.check_closed(self.player):
-                DarkBomb2Game.closed_bomb = True
-                break
+            """ Check Closed """
+            for bomb in self.bombs:
+                if bomb.check_closed(self.player):
+                    DarkBomb2Game.closed_bomb = True
+                    break
 
-        """ Restart When Game Over """
-        if DarkBomb2Game.game_over:
-            print 'game over' 
-            self.init_board(DarkBomb2Game.WHITE)
-        if self.is_key_pressed(K_SPACE):
-            DarkBomb2Game.game_over = False
-            self.reinit()
+            """ Restart When Game Over """
+            if DarkBomb2Game.game_over:
+                print 'game over' 
+                self.init_board(DarkBomb2Game.WHITE)
+            if self.is_key_pressed(K_SPACE):
+                DarkBomb2Game.game_over = False
+                self.reinit()
 
-        """ Add Bomb When Game Finished """    
-        if DarkBomb2Game.game_finish:
-            print 'you win ' + str(len(self.bombs))
-            self.init_player(DarkBomb2Game.YELLOW)
-            self.bombs.append(Bomb(radius = 20,
-                        color = DarkBomb2Game.BLACK, 
-                        pos = (self.window_size[0]/2,self.window_size[1]/2))
-                        )
-            DarkBomb2Game.game_finish = False
+            """ Add Bomb When Game Finished """    
+            if DarkBomb2Game.game_finish:
+                print 'you win ' + str(len(self.bombs))
+                self.init_player(DarkBomb2Game.YELLOW)
+                self.bombs.append(Bomb(radius = 20,
+                            color = DarkBomb2Game.BLACK, 
+                            pos = (self.window_size[0]/2,self.window_size[1]/2))
+                            )
+                DarkBomb2Game.game_finish = False
 
-        """ Warning When Closed """
-        if DarkBomb2Game.closed_bomb:
-            self.player = Player(radius = 20, color = DarkBomb2Game.RED, 
-                                pos = (self.player.x, self.player.y))
-            DarkBomb2Game.closed_bomb = False
-        else:
-            self.player = Player(radius = 20, color = DarkBomb2Game.YELLOW, 
-                                pos = (self.player.x, self.player.y))
+            """ Warning When Closed """
+            if DarkBomb2Game.closed_bomb:
+                self.player = Player(radius = 20, color = DarkBomb2Game.RED, 
+                                    pos = (self.player.x, self.player.y))
+                DarkBomb2Game.closed_bomb = False
+            else:
+                self.player = Player(radius = 20, color = DarkBomb2Game.YELLOW, 
+                                    pos = (self.player.x, self.player.y))
 
-        """ Render Bombs' Number """
-        self.score_image = self.font.render('Bomb = %d' % len(self.bombs), 0, DarkBomb2Game.BLUE)
+            """ Render Bombs' Number """
+            self.score_image = self.font.render('Bomb = %d' % len(self.bombs), 0, DarkBomb2Game.BLUE)
 
     def render(self, surface):
         pygame.draw.rect(surface, DarkBomb2Game.GREEN, pygame.Rect(self.window_size[0] - self.player.radius,
